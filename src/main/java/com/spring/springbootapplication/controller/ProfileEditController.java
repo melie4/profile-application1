97fcf00867adf61aceb56583d74e91061f42967e
melie4 <hanameli@m04.itscom.net>
2026-08-22 21:21:13 +0900

    自己紹介編集機能の実装

---
+++ src/main/java/com/spring/springbootapplication/controller/ProfileEditController.java
@@ -0,0 +1,82 @@
+package com.spring.springbootapplication.controller;
+
+import com.spring.springbootapplication.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+
+import com.spring.springbootapplication.dto.ProfileEditDto;
+import com.spring.springbootapplication.entity.User;
+import com.spring.springbootapplication.security.LoginUserDetails;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Controller
+public class ProfileEditController {
+    
+    private final UserService userService;
+
+    ProfileEditController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/profile-edit")
+    public ModelAndView index(
+        @ModelAttribute("userModel") ProfileEditDto form,
+        @AuthenticationPrincipal LoginUserDetails loginUser,
+        ModelAndView mav,
+        HttpServletRequest request){
+            mav.setViewName("/profile-edit");
+            
+            //DBに登録済みの自己紹介文を取得する
+            User loginUserInfo = loginUser.getUser();
+            form.setSelfIntroduction(
+            loginUserInfo.getSelfIntroduction()
+            );
+
+           String userName = request.getRemoteUser();
+           mav.addObject("userName", userName + "  portfolio site");
+
+           return mav;
+    
+    }
+
+    @RequestMapping(value = "/profile-edit", method= RequestMethod.POST)
+    public ModelAndView form(
+        @ModelAttribute("userModel") @Validated ProfileEditDto formUser,
+        BindingResult result,
+        @RequestParam("avatarImage") MultipartFile avatarImage,
+        @AuthenticationPrincipal LoginUserDetails loginUser,
+        HttpServletRequest request,
+        ModelAndView mav) throws IOException{
+            
+        if(result.hasErrors()){
+        mav.setViewName("profile-edit");
+        
+        String userName = request.getRemoteUser();
+        mav.addObject("userName", userName + "  portfolio site");
+       
+        return mav;
+        }
+
+        User loginUserInfo = loginUser.getUser();
+
+        userService.updateProfile(loginUserInfo,avatarImage,formUser);
+    
+        return new ModelAndView("redirect:/top");
+    
+    }
+    
+
+}
--- src/main/java/com/spring/springbootapplication/service/UserService.java
+++ src/main/java/com/spring/springbootapplication/service/UserService.java
@@ -1,14 +1,23 @@
 package com.spring.springbootapplication.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import com.spring.springbootapplication.entity.User;
 import com.spring.springbootapplication.repository.UserRepository;
+import com.spring.springbootapplication.dto.ProfileEditDto;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import jakarta.transaction.Transactional;
@@ -42,4 +51,46 @@ public class UserService {
         
         repository.saveAndFlush(user);
     }
+
+    //自己紹介編集
+    public void updateProfile(
+        User loginUserInfo,
+        MultipartFile avatarImage,
+        ProfileEditDto formUser
+        )
+        throws IOException{
+
+         //自己紹介文の設定
+        loginUserInfo.setSelfIntroduction(
+        formUser.getSelfIntroduction()
+        );
+
+        if(!avatarImage.isEmpty()){
+          
+        //画像の設定
+        String originalFileName = avatarImage.getOriginalFilename();
+
+        String fileName =
+            UUID.randomUUID() + "-" + originalFileName;
+
+        Path uploadDir = Paths.get("profile-images");
+        Files.createDirectories(uploadDir);
+
+        Path imagePath = uploadDir.resolve(fileName);
+
+        Files.copy(
+            avatarImage.getInputStream(),
+            imagePath,
+            StandardCopyOption.REPLACE_EXISTING
+        );
+
+        loginUserInfo.setAvatarImage(fileName);
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        loginUserInfo.setUpdateAt(now);
+
+        repository.saveAndFlush(loginUserInfo);
+
+        }
 }
